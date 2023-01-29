0d2c11606c121fff35c408f4a84d35d562343fa7
Rieksts <karlis.rieksts@sap.com>
2023-01-29 20:04:48 +0100

    progress with section 18 - forkify app

---
+++ UD_complete_JavaScript_course/18-forkify/src/js/config.js
@@ -0,0 +1,5 @@
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10;
+// 1efcd1ae-fa04-478f-bfa2-0d4ef8123a79
--- UD_complete_JavaScript_course/18-forkify/src/js/controller.js
+++ UD_complete_JavaScript_course/18-forkify/src/js/controller.js
@@ -1,13 +1,55 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from "./views/recipeView";
+import searchView from "./views/searchView";
+import resultsView from "./views/resultsView";
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import {loadSearchResults} from "./model.js";
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+if(module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function() {
+  try {
+    // Read the current hash:
+    const id = window.location.hash.slice(1);
+
+    // Guard clause in case the url is without a hash
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading the recipe
+    await model.loadRecipe(id);
+
+    // 2. Render recipe
+    recipeView.render(model.state.recipe)
+
+  } catch(err) {
+    recipeView.renderError();
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlSearchResults = async function() {
+  try {
+    resultsView.renderSpinner();
 
-///////////////////////////////////////
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if(!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render results
+    resultsView.render(model.state.search.results);
+  } catch(err) {
+    console.log(err)
+  }
+};
+
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
